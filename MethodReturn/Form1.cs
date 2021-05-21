0310ffdcf6e1bafaed1c6e8be679c58e33068283
MaeveWheaton <teamheaven10@outlook.com>
2021-05-21 09:48:53 -0400

    coded

--- MethodReturn/Form1.cs
+++ MethodReturn/Form1.cs
@@ -33,9 +33,8 @@ namespace MethodReturn
             int num1 = Convert.ToInt16(q1Num1Input.Text);
             int num2 = Convert.ToInt16(q1Num2Input.Text);
            
-            Add(num1, num2);
-
-
+            int answer = Add(num1, num2);
+            q1Output.Text = $"{num1} + {num2} = {answer}";
 
             /// 2.  Get values for question 2 and call the Area method. 
             /// Accept the returned value and then display it.
@@ -48,7 +47,11 @@ namespace MethodReturn
             /// Input: 6, 2
             /// The area is 18 units squared
 
+            int width = Convert.ToInt16(widthInput.Text);
+            int length = Convert.ToInt16(lengthInput.Text);
 
+            int result = Area(length, width);
+            q2Output.Text = $"The area is {result} units squared";
 
             /// 3.  Get a value for question 3 and call the PrintPrice 
             /// method. Accept the returned value and then display it.
@@ -64,28 +67,32 @@ namespace MethodReturn
             /// Input: 200
             /// The total price is $100.00
 
+            int prints = Convert.ToInt32(copiesInput.Text);
 
-
+            double cost = PrintPrice(prints);
+            q3Output.Text = $"The total price is {cost.ToString("C")}";
         }
 
         /// 1. Create a method called Add, that accepts 2 int 
         /// parameters, (x and y), adds them together, and then 
         /// returns the result back to the method call.       
 
-        public void Add(int x, int y)
+        public int Add(int x, int y)
         {
             int sum = x + y;
-
-            q1Output.Text = $"{x} + {y} = {sum}";
+            return sum;
         }
-
 
         /// 2. Create a method called Area, that accepts 2 int 
         /// parameters, (x and y), uses them to determine the 
         /// area of a rectangle , and then returns the result 
         /// back to the method call.
 
-
+        public int Area(int length, int width)
+        {
+            int area = length * width;
+            return area;
+        }
 
         /// 3. Create a method called PrintPrice, that accepts 1 
         /// int parameter, (prints), uses it to determine the 
@@ -98,6 +105,28 @@ namespace MethodReturn
         /// 51 - 100 prints:  $0.65/print
         /// 101 or over:  $0.50/print
 
+        public double PrintPrice(int prints)
+        {
+            double price;
 
+            if (prints <= 10)
+            {
+                price = prints * 1;
+            }
+            else if (prints <= 50)
+            {
+                price = prints * 0.75;
+            }
+            else if (prints <= 100)
+            {
+                price = prints * 0.65;
+            }
+            else
+            {
+                price = prints * 0.50;
+            }            
+
+            return price;
+        }
     }
 }
